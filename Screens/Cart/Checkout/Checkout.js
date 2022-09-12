721b22ad589693d092b521fb7d7e2a08f207bfe7
ArgusXronos <jonatanrivera@gmail.com>
2022-09-11 19:53:56 -0500

    Shipping Page

--- Screens/Cart/Checkout/Checkout.js
+++ Screens/Cart/Checkout/Checkout.js
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Text } from "react-native";
-import { Button, Item, Select, View } from "native-base";
+import {
+  Button,
+  Item,
+  Select,
+  View,
+  CheckIcon,
+  NativeBaseProvider,
+} from "native-base";
 import { Icon } from "react-native-vector-icons/FontAwesome";
 
 import FormContainer from "../../../Shared/Form/FormContainer";
 import Input from "../../../Shared/Form/Input";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-import { connect, Connect } from "react-redux";
+import { connect } from "react-redux";
 
 const countries = require("../../../assets/data/countries.json");
 
@@ -19,14 +26,12 @@ const Checkout = (props) => {
   const [zip, setZip] = useState();
   const [country, setCountry] = useState();
   const [phone, setPhone] = useState();
-
   useEffect(() => {
     setOrderItems(props.cartItems);
     return () => {
       setOrderItems();
     };
   }, []);
-
   const checkOut = () => {
     let order = {
       city,
@@ -38,74 +43,88 @@ const Checkout = (props) => {
       shippingAddress2: address2,
       zip,
     };
-
     props.navigation.navidate("Payment", { order: order });
   };
-
   return (
-    <KeyboardAwareScrollView
-      viewIsInsideTabBar={true}
-      extraHeight={200}
-      enableOnAndroid={true}
-    >
-      <FormContainer title={"Shipping Address"}>
-        <Input
-          placeholder={"Phone"}
-          name={"phone"}
-          value={phone}
-          keyboardtype={"numeric"}
-          onChangeText={(text) => setPhone(text)}
-        />
-        <Input
-          placeholder={"Shipping Adrees 1"}
-          name={"ShippingAddress1"}
-          value={address}
-          onChangeText={(text) => setAddress(text)}
-        />
-        <Input
-          placeholder={"Shipping Adrees 2"}
-          name={"ShippingAddress2"}
-          value={address2}
-          onChangeText={(text) => setAddress2(text)}
-        />
-        <Input
-          placeholder={"City"}
-          name={"city"}
-          value={city}
-          onChangeText={(text) => setCity(text)}
-        />
-        <Input
-          placeholder={"Zip Code"}
-          name={"zip"}
-          value={zip}
-          keyboardtype={"numeric"}
-          onChangeText={(text) => setZip(text)}
-        />
-        <Select
-          selectedValue={country}
-          minWidth="200"
-          accessibilityLabel="Choose Country"
-          placeholder="Choose Country"
-          _selectedItem={{
-            bg: "teal.600",
-            endIcon: <CheckIcon size="5" />,
-          }}
-          mt={1}
-          onValueChange={(itemValue) => setCountry(itemValue)}
-        >
-          {countries.map((itemCountry) => (
-            <Select.Item
-              key={itemCountry.code}
-              label={itemCountry.name}
-              value={itemCountry.name}
-            />
-          ))}
-        </Select>
-        <View style={{ width: "80%", alignItems: "center" }}>
-          <Button title="Confirm" onPress={() => checkOut()} />
-        </View>
-      </FormContainer>
-    </KeyboardAwareScrollView>
+    <NativeBaseProvider>
+      <KeyboardAwareScrollView
+        viewIsInsideTabBar={true}
+        extraHeight={200}
+        enableOnAndroid={true}
+      >
+        <FormContainer title={"Shipping Address"}>
+          <Input
+            placeholder={"Phone"}
+            name={"phone"}
+            value={phone}
+            keyboardtype={"numeric"}
+            onChangeText={(text) => setPhone(text)}
+          />
+          <Input
+            placeholder={"Shipping Adrees 1"}
+            name={"ShippingAddress1"}
+            value={address}
+            onChangeText={(text) => setAddress(text)}
+          />
+          <Input
+            placeholder={"Shipping Adrees 2"}
+            name={"ShippingAddress2"}
+            value={address2}
+            onChangeText={(text) => setAddress2(text)}
+          />
+          <Input
+            placeholder={"City"}
+            name={"city"}
+            value={city}
+            onChangeText={(text) => setCity(text)}
+          />
+          <Input
+            placeholder={"Zip Code"}
+            name={"zip"}
+            value={zip}
+            keyboardtype={"numeric"}
+            onChangeText={(text) => setZip(text)}
+          />
+          <Select
+            selectedValue={country}
+            minWidth="200"
+            accessibilityLabel="Choose Country"
+            placeholder="Choose Country"
+            _selectedItem={{
+              bg: "teal.600",
+              endIcon: <CheckIcon size="5" />,
+            }}
+            mt={1}
+            onValueChange={(itemValue) => setCountry(itemValue)}
+          >
+            {countries.map((itemCountry) => (
+              <Select.Item
+                key={itemCountry.code}
+                label={itemCountry.name}
+                value={itemCountry.name}
+              />
+            ))}
+          </Select>
+          <View
+            borderWidth={1}
+            style={{ width: "80%", alignItems: "center", marginTop: 20 }}
+          >
+            <Button
+              iconLeft
+              dark
+              width={"100%"}
+              height={50}
+              title="Confirm"
+              color={"white"}
+              onPress={() => checkOut()}
+            >
+              {/* <Icon name="cog" /> */}
+              <Text>Confirm</Text>
+            </Button>
+          </View>
+        </FormContainer>
+      </KeyboardAwareScrollView>
+    </NativeBaseProvider>
   );
 };
 
